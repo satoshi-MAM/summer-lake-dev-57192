dce6c760c3e15cd8f45131c29281fa7ae108ce0c
Crowdbotics <engineering@crowdbotics.com>
2023-01-16 09:34:29 +0000

    Initial commit.

---
+++ backend/summer_lake_dev_57192/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for summer_lake_dev_57192 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'summer_lake_dev_57192.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "summer-lake-dev-57192",
+  url: "https://summer-lake-dev-57192.botics.co",
+  api: "https://summer-lake-dev-57192.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
